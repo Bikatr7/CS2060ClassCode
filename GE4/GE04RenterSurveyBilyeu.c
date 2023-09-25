f243790a10d6229db127fc3fa78a884045e673b0
Bikatr7 <Tetralon07@gmail.com>
2023-09-24 19:47:47 -0600

    Added print categories.
    return added, docstring as well

--- GE4/GE04RenterSurveyBilyeu.c
+++ GE4/GE04RenterSurveyBilyeu.c
@@ -12,4 +12,32 @@ void printCategoryData(float categoryAverages[], int numCategories);
 int main()
 {
 
+	return 0;
+
+}
+
+void printCategories(const char* categories[], size_t totalCategories) 
+{
+
+    /*
+    * 
+    * Print the categories for the survey.
+    * 
+    * Parameters:
+    * const char* categories[]: The array of categories for the survey.
+    * size_t totalCategories: The total number of categories in the survey.
+    * 
+    * Returns:
+    * Void.
+    * 
+    */
+
+    printf("Rating Categories:\t");
+
+    for (size_t i = 0; i < totalCategories; ++i) 
+    {
+        printf("\t%zu.%s\t", i + 1, categories[i]);
+    }
+
+    printf("\n");
 }
