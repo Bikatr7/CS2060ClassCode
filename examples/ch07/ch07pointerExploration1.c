2fb6563e6c44eb094a7f1f5e24508fd286168cb0
Bikatr7 <Tetralon07@gmail.com>
2023-10-12 16:57:22 -0600

    added explanatory comments to pointer exploration

--- examples/ch07/ch07pointerExploration1.c
+++ examples/ch07/ch07pointerExploration1.c
@@ -15,24 +15,32 @@ int main ()
 	int calcHouseNum2 = 0;
 	int *houseNumPtr = &houseNum;
 	
+	// prints raw var
 	printf("houseNum %d\n", houseNum);
 
+	// prints var address
 	printf("&houseNum %p\n\n", &houseNum);
 
+	// prints int value at the pointer address
 	printf ("*houseNumPtr  %d\n", *houseNumPtr);
 
+	// prints the address of the value the pointer is pointing to
 	printf ("houseNumPtr %p\n\n", houseNumPtr);
 
+	// prints the address of the pointer
 	printf ("&houseNumPtr %p\n\n", &houseNumPtr);
 
+	// adds the values that the pointer is pointing to together
 	calcHouseNum1 =  *houseNumPtr + *houseNumPtr;
 
 	printf("*houseNumPtr + *houseNumPtr is %d\n\n", calcHouseNum1);
 
+	// same as above but with a different method
 	calcHouseNum2 = 2 * (*houseNumPtr);
 
 	printf("2 * (*houseNumPtr) is %d\n\n", calcHouseNum2);
 
+	// shows that * and & precedence does not matter
 	printf("\n&*houseNumPtr = %p"
 		"\n*&houseNumPtr = %p\n", &*houseNumPtr, *&houseNumPtr);
 			
